c1d530fa84ffc9a408934d4949de33704438d9ef
charitarthchugh <37895518+charitarthchugh@users.noreply.github.com>
2020-09-07 15:43:28 -0400

    Create install.sh to quickly install dotfiles

---
+++ install.sh
@@ -0,0 +1,4 @@
+ln -s /home/ccd12/dotfiles/theming/firefox/MaterialFox/user.js /home/ccd12/.mozilla/firefox/school/user.js
+ln -s /home/ccd12/dotfiles/theming/firefox/MaterialFox/user.js /home/ccd12/.mozilla/firefox/Personal/user.js
+ln -s /home/ccd12/dotfiles/theming/firefox/MaterialFox/chrome /home/ccd12/.mozilla/firefox/Personal/chrome
+ln -s /home/ccd12/dotfiles/theming/firefox/MaterialFox/chrome /home/ccd12/.mozilla/firefox/school/chrome
