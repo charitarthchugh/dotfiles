6a3a5ba3dde56c1a84ad1843a5ec3cbae9881b0d
charitarthchugh <37895518+charitarthchugh@users.noreply.github.com>
2021-02-06 18:47:08 -0500

    delete install.sj- replaced by install(dotbot)

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-ln -s $HOME/dotfiles/theming/firefox/MaterialFox/user.js $HOME/.mozilla/firefox/school/user.js || return 1
-ln -s $HOME/dotfiles/theming/firefox/MaterialFox/user.js $HOME/.mozilla/firefox/Personal/user.js || return 1
-ln -s $HOME/dotfiles/theming/firefox/MaterialFox/chrome $HOME/.mozilla/firefox/Personal/chrome || return 1
-ln -s $HOME/dotfiles/theming/firefox/MaterialFox/chrome $HOME/.mozilla/firefox/school/chrome || return 1
-antibody bundle < ~/dotfiles/.zsh_plugins.txt > ~/dotfiles/.zsh_plugins.sh
-
