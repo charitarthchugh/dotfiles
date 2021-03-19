ebc4aa7395868f8cc526d5de6954ca4bc64c70f2
charitarthchugh <37895518+charitarthchugh@users.noreply.github.com>
2021-03-19 16:28:44 -0400

    config.fish now uses $HOME instead of full paths

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,8 @@
 
-source ~/.config/up/up.fish
+source $HOME/.config/up/up.fish
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-eval /home/ccd12/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+eval $HOME/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
 export BETTER_EXCEPTIONS=1
 # Homebrew completions
