2afffac802dd77a2a9346c512f113d48adaa90d2
charitarthchugh <37895518+charitarthchugh@users.noreply.github.com>
2021-07-01 22:14:06 -0400

    make conda init less annoying

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,8 @@
 
 source $HOME/.config/up/up.fish
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-eval $HOME/miniconda3/bin/conda "shell.fish" "hook" $argv | source
-# <<< conda initialize <<<
+if test -d $HOME/miniconda3
+    eval $HOME/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+end
 export BETTER_EXCEPTIONS=1
 
 starship init fish | source
