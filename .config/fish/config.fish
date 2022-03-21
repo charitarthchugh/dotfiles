483f91d68c21de37ef7210de51d5b0cd879386dd
charitarthchugh <37895518+charitarthchugh@users.noreply.github.com>
2022-03-20 22:29:56 -0400

    Stop funcsave from occuring on startup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,10 @@
-source $HOME/.config/up/up.fish
+#source $HOME/.config/up/up.fish
 source $HOME/dotfiles/.env.fish
 if test -d $HOME/miniconda3
-    eval $HOME/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+    eval $HOME/miniconda3/bin/conda "shell.fish" hook $argv | source
 end
 export BETTER_EXCEPTIONS=1
 fzf_configure_bindings
 complete --no-files --command pipes --arguments '(pipes --_completion (commandline -cp))'
 starship init fish | source
-starship completions fish | source 
+starship completions fish | source
