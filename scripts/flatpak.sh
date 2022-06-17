9f493f6499b9a48d2569bc2baf7930b10ae2c458
charitarthchugh <37895518+charitarthchugh@users.noreply.github.com>
2022-06-17 18:11:51 -0400

    script flatpak installations

---
+++ scripts/flatpak.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# Set vars
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Add flathub repo
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+# Disable any filters, if present
+flatpak remote-modify flathub --no-filter                
+
+# Install flatpak apps
+while read package; do
+	yes | flatpak install flathub "$package"
+	if [[ $? -eq 0 ]]; then
+		echo "$package is installed from flatpak!"
+	else
+		echo "$package" >>"${BASEDIR}/flatpak_failed.txt"
+	fi
+done <"${BASEDIR}/../packages/flatpak.txt"
