db67c7ed0e38ad044c5e64ded1ba6d6384f52574
charitarthchugh <37895518+charitarthchugh@users.noreply.github.com>
2022-06-17 01:45:01 -0400

    feat(nvim): add lsp servers

--- .config/nvim/lua/user/lsp/configs.lua
+++ .config/nvim/lua/user/lsp/configs.lua
@@ -5,7 +5,7 @@ end
 
 local lspconfig = require("lspconfig")
 
-local servers = { "jsonls", "sumneko_lua" }
+local servers = { "jsonls", "sumneko_lua", "bashls", "html", "eslint", "tsserver", "yamlls", "pyright", "grammarly", "ltex", "lemminx"}
 
 lsp_installer.setup {
 	ensure_installed = servers
