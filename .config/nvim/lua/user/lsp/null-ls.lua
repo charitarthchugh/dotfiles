fbcde89b2beda39db74fce84df5aeafc5938e2a0
charitarthchugh <37895518+charitarthchugh@users.noreply.github.com>
2022-09-02 20:47:29 -0400

    Update neovim config

--- .config/nvim/lua/user/lsp/null-ls.lua
+++ .config/nvim/lua/user/lsp/null-ls.lua
@@ -7,7 +7,7 @@ end
 local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
-
+local code_actions = null_ls.builtins.code_actions
 null_ls.setup({
 	debug = false,
 	sources = {
@@ -19,6 +19,12 @@ null_ls.setup({
 		formatting.dart_format,
 		formatting.isort,
 		formatting.markdownlint,
+    formatting.clang_format,
+    formatting.codespell,
+    formatting.latexindent,
+    code_actions.gitsigns,
+    code_actions.proselint
+
 		-- diagnostics.flake8
 	},
 })
