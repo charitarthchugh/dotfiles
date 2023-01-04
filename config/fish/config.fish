38cacaa7dd8a820d55c6d0402b9d3093dfd5a074
charitarthchugh <37895518+charitarthchugh@users.noreply.github.com>
2023-01-04 15:12:44 -0500

    update/cleanup config.fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,9 +1,17 @@
-source $HOME/dotfiles/.env.fish
+if test -d $HOME/dotfiles
+    source $HOME/dotfiles/.env.fish
+end
 if test -d $HOME/miniconda3
     eval $HOME/miniconda3/bin/conda "shell.fish" hook $argv | source
 end
+if test -d /home/linuxbrew
+    eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+end
 export BETTER_EXCEPTIONS=1
-fzf_configure_bindings
-complete --no-files --command pipes --arguments '(pipes --_completion (commandline -cp))'
-starship init fish | source
-starship completions fish | source
+if command -v fzf > /dev/null
+    fzf_configure_bindings
+end
+if command -v starship > /dev/null
+    starship init fish | source
+    starship completions fish | source
+end
