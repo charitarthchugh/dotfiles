78494d63d5ea50171d3bc1886b4c32e7f5d26faa
charitarthchugh <37895518+charitarthchugh@users.noreply.github.com>
2023-01-14 12:59:44 -0500

    enable RPMFusion and install some packages for firefox

--- scripts/dnf-gui.sh
+++ scripts/dnf-gui.sh
@@ -15,6 +15,8 @@ EOF
 #VSCode Repo
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+# RPMFusion
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 sudo dnf update -y
 # Install dnf packages (gui only)
