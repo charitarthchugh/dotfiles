c86692bc62869971a18a9c84774c750704e28df0
Charitarth Chugh <37895518+charitarthchugh@users.noreply.github.com>
2025-05-08 21:32:17 -0400

    Simplifies extension list generation for gh

--- scripts/list-gh-ext.sh
+++ scripts/list-gh-ext.sh
@@ -2,7 +2,6 @@
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 rm "$BASEDIR/../packages/gh-extensions.txt"
 while read -r line; do
-    a=($line)
     for word in $line; do 
         if [[ $word == *"/"* ]]; then 
             echo $word >> "$BASEDIR/../packages/gh-extensions.txt"
