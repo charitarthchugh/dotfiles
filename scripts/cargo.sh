44b23f2b24ddd56517595fd5b30b9d62bfa2e0ff
Charitarth Chugh <37895518+charitarthchugh@users.noreply.github.com>
2025-05-08 20:49:28 -0400

    Add cargo binary install script

---
+++ scripts/cargo.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+if command -v cargo > /dev/null 2>&1;then
+  echo 'using existing cargo'
+else
+  echo "Installing cargo via rustup"
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --profile minimal
+  source ~/.cargo/bin/env
+fi
+
+while read package; do
+    cargo install "$package" >> /dev/null
+    if [[ $? -eq 0 ]]; then
+        echo "$package is installed from gh!"
+    else
+        echo "$package" >> "$BASEDIR/../gh_failed.txt"
+    fi
+done < "$BASEDIR/../packages/cargo.txt"
