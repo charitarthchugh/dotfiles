c4a7e48cb4c24f319e8bfbf048b1b524a0fbbf0f
charitarthchugh <37895518+charitarthchugh@users.noreply.github.com>
2025-09-20 19:47:09 -0400

    Refactor `dnf-cli.sh`: remove redundant tailscaled enable command.

--- scripts/dnf-cli.sh
+++ scripts/dnf-cli.sh
@@ -24,5 +24,3 @@ nix-channel --update
 export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}
 nix-shell '<home-manager>' -A install
 home-manager switch
-
-sudo systemctl enable --now tailscaled
